6e26d4ba5d982b3600e08dc7e678a2c8d0721ca4
Toni Sharpe <toni.sharpe@nearform.com>
2021-02-03 16:00:22 +0100

    Step 3 is cleaned up, it should be like step 2 but with logs added
    Relates to issue #7

--- src/step-3/routes/users.js
+++ src/step-3/routes/users.js
@@ -1,15 +1,5 @@
-import S from 'fluent-json-schema'
-
-const schema = {
-  response: {
-    200: S.array().items(
-      S.object().prop('username', S.string().required())
-    ),
-  },
-}
-
 export default async function users(fastify) {
-  fastify.get('/users', { schema }, async () => {
+  fastify.get('/users', {}, async () => {
     fastify.log.info('Users endpoint called.')
     return [{ username: 'alice' }, { username: 'bob' }]
   })
