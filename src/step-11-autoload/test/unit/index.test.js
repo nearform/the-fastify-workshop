da3ff936d92cf13202945ccefee6d38ef93d311f
Luke Jones <lukejones@LJones-UKMAC21.local>
2021-05-20 13:24:30 +0100

    Rename folder steps to use 2 digits

---
+++ src/step-11-autoload/test/unit/index.test.js
@@ -0,0 +1,15 @@
+import { test } from 'tap'
+
+import buildServer from '../index.js'
+import config from '../config.js'
+
+test('Startup', async t => {
+  t.test('it registers the JWT plugin', async t => {
+    const fastify = buildServer(config)
+
+    await fastify.ready()
+
+    t.ok(fastify.jwt)
+  })
+})
+
--- src/step-9-decorators/README.md
+++
@@ -1,6 +0,0 @@
-# step-9
-
-## Setup
-
-- start the server with `yarn start`
-- run the tests with `yarn test`
