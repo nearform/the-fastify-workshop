e70b8dd502c17045f919d55418db9c0c70ebdb19
brainrepo <murru7@gmail.com>
2022-03-02 09:33:24 +0100

    fix: solve tast fail creating a test for the login

---
+++ src/step-05-testing/test/login.test.js
@@ -0,0 +1,42 @@
+import t from 'tap'
+import fastify from 'fastify'
+
+const { test } = t
+
+import buildServer from '../index.js'
+
+test('POST /login', async t => {
+  t.test('returns 400 with an invalid post payload', async t => {
+    const fastify = buildServer()
+
+    const res = await fastify.inject({
+      url: '/login',
+      method: 'POST',
+      body: {
+        name: 'alice',
+        passcode: 'alice',
+      },
+    })
+
+    t.equal(res.statusCode, 400)
+  })
+
+  t.test('returns the data with valid post payload', async t => {
+    const fastify = buildServer()
+
+    const res = await fastify.inject({
+      url: '/login',
+      method: 'POST',
+      body: {
+        username: 'alice',
+        password: 'alice',
+      },
+    })
+
+    t.equal(res.statusCode, 200)
+    t.same(await res.json(), {
+      username: 'alice',
+      password: 'alice',
+    })
+  })
+})
