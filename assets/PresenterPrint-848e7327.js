b0c609b3875a3ba98150cf31c68fa606610ef7f7
GergoHorvathNearForm <GergoHorvathNearForm@users.noreply.github.com>
2023-08-16 11:02:40 +0000

    deploy: 8908f2cfdbd91234e46a361a2e8bc3d8503d0d2d

--- assets/PresenterPrint-848e7327.js
+++ assets/PresenterPrint-848e7327.js
@@ -1,4 +1,4 @@
-import{d as _,i as d,a as h,u as p,b as u,c as m,e as f,f as n,g as t,t as s,h as a,F as g,r as v,n as x,j as y,o as r,k as b,l as k,m as w,p as N,q as P,_ as S}from"./index-6dbc0a18.js";import{N as V}from"./NoteDisplay-45866f23.js";const j={class:"m-4"},L={class:"mb-10"},T={class:"text-4xl font-bold mt-2"},B={class:"opacity-50"},C={class:"text-lg"},D={class:"font-bold flex gap-2"},H={class:"opacity-50"},z=t("div",{class:"flex-auto"},null,-1),F={key:0,class:"border-gray-400/50 mb-8"},M=_({__name:"PresenterPrint",setup(q){d(h),p(`
+import{d as _,i as d,a as h,u as p,b as u,c as m,e as f,f as n,g as t,t as s,h as a,F as g,r as v,n as x,j as y,o as r,k as b,l as k,m as w,p as N,q as P,_ as S}from"./index-0a4ab130.js";import{N as V}from"./NoteDisplay-a8e732da.js";const j={class:"m-4"},L={class:"mb-10"},T={class:"text-4xl font-bold mt-2"},B={class:"opacity-50"},C={class:"text-lg"},D={class:"font-bold flex gap-2"},H={class:"opacity-50"},z=t("div",{class:"flex-auto"},null,-1),F={key:0,class:"border-gray-400/50 mb-8"},M=_({__name:"PresenterPrint",setup(q){d(h),p(`
 @page {
   size: A4;
   margin-top: 1.5cm;
