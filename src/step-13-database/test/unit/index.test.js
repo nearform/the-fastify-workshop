f3d7f0ea464cc0ffa270ccaae4cfa1dc4d812f74
Reaper Gelera <ahoy@barelyhuman.dev>
2024-02-01 14:33:28 +0530

    refactor(tests): switch to assert/strict

--- src/step-13-database/test/unit/index.test.js
+++ src/step-13-database/test/unit/index.test.js
@@ -1,5 +1,5 @@
 import { test } from 'node:test'
-import assert from 'node:assert'
+import assert from 'node:assert/strict'
 
 import buildServer from '../../index.js'
 import config from '../../config.js'
