bdc02bc6a0381bf73891a04438066741db0d47db
lv10 <lv10@users.noreply.github.com>
2026-08-24 16:07:37 +0000

    deploy: 602161d6d7a8fd1175217f2bd9abfdf1474772ec

---
+++ assets/slidev/default-B3OGPh1e.js
@@ -0,0 +1,1 @@
+import{B as e,L as t,y as n}from"../modules/shiki-DeTF74yl.js";import{vt as r}from"../index-BN2eLK_k.js";var i={},a={class:`slidev-layout default`};function o(r,i){return t(),n(`div`,a,[e(r.$slots,`default`)])}var s=r(i,[[`render`,o]]);export{s as t};
