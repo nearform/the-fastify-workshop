67a2cf7d8ec96581e4d4725ebbd0728d8b4270b8
SociableSteve <SociableSteve@users.noreply.github.com>
2026-09-22 11:29:19 +0000

    deploy: 1886d74e31d43b4a025d98f1e9854013b2a560e1

---
+++ assets/slidev/default-Bf0UJseI.js
@@ -0,0 +1,1 @@
+import{B as e,L as t,y as n}from"../modules/shiki-CHeudtFl.js";import{vt as r}from"../index-Cliibo0p.js";var i={},a={class:`slidev-layout default`};function o(r,i){return t(),n(`div`,a,[e(r.$slots,`default`)])}var s=r(i,[[`render`,o]]);export{s as t};
